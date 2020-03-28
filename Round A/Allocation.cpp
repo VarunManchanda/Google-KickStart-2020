c52bc5d9713cb50c441407872a484566fe7847f2
Varun Manchanda <33371013+VarunManchanda@users.noreply.github.com>
2020-03-28 13:08:40 +0530

    Create Allocation.cpp

---
+++ Round A/Allocation.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _fast_ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int solve()
+{
+    int n; cin >> n; int b; cin >> b;
+    int s = 0; int c = 0;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    for(auto i : arr)
+    {
+        s+=i;
+        if(s<=b)
+        {
+            c+=1;
+        }
+    }
+    return c;
+}
+
+
+
+
+int main()
+{
+    _fast_;
+    int t; cin >> t;
+    int i = 1;
+    while(t--)
+    {
+        cout << "Case #" << i << ": " << solve() << endl;
+        i+=1;
+    }
+    return 0;
+}
